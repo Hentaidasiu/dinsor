4db906d6277efa83731eddd61d7fde54cf757c59
unknown <hentaidasu@gmail.com>
2020-05-28 23:02:52 +0700

    schemaChange

--- model/user.js
+++ model/user.js
@@ -1,11 +1,11 @@
 const   mongoose = require('mongoose'),
         passportLocalMongoose = require('passport-local-mongoose');
 
-let dinsorSchema = new mongoose.Schema({
+let userSchema = new mongoose.Schema({
     username : String,
     password : String
 });
 
-dinsorSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', dinsorSchema);
+module.exports = mongoose.model('User', userSchema);
